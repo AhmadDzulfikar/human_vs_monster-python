b002ce3eacea45b9b11e57491bd9400c6fd21bc9
Ahmad Dzulfikar <dzulfikar19.ahmad@gmail.com>
2023-06-22 23:47:29 +0700

    monster nyerang balik

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 class Player:
-    def __init__(self, health, energy=100): # constructor
+    def __init__(self, health=200, energy=100): # constructor
         self.health = health
         self.energy = energy
-        print(f"player created")
 
-    def attack(self, damage = 1):
-        self.target = monster.health
+    def attack(self, target, damage = 1):
+        target.health -= damage 
         self.energy -= damage #self.energy = self.energy - damage
-        print("attacking!!!")
+        if target.is_attacked():
+            self.health -= target.damage
+        print(f"attacking to monster, monster health : {target.health} left, your energy is {self.energy} left" )
 
 class Monster:
     def __init__(self, health = 100, ):
-        self.health = health
-        print("monster created")
+        self.health = health # dinamis
+        self.health_init = self.health # always 500
+    
+    def is_attacked(self):
+        return self.health < self.health_init
+        
 
 
+player1 = Player()
+player2 = Player()
+oni = Monster(health=500)
 
-player = Player(health=200)
-monster = Monster(health=500)
+player1.attack(target=oni, damage=80)
+player2.attack(target=oni, damage=40)
 
-player.attack(damage=80,)
+oni.attack()
+
+
 
 
 
