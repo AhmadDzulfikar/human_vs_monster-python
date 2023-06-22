4524d392f7d5db841a054f7501f3200745629f35
Ahmad Dzulfikar <dzulfikar19.ahmad@gmail.com>
2023-06-23 00:21:29 +0700

    finish game player vs monster

--- main.py
+++ main.py
@@ -1,33 +1,48 @@
 class Player:
-    def __init__(self, health=200, energy=100): # constructor
+    def __init__(self, name, health=200, energy=100): # constructor
+        self.name = name
         self.health = health
         self.energy = energy
 
     def attack(self, target, damage = 1):
         target.health -= damage 
         self.energy -= damage #self.energy = self.energy - damage
-        if target.is_attacked():
+        print(f"{self.name} attacking {damage} damage to {target.name}! ")
+        if target.is_attacked(player_name=self.name):
             self.health -= target.damage
-        print(f"attacking to monster, monster health : {target.health} left, your energy is {self.energy} left" )
+
+    def show_info(self):
+        print(f"{self.name} health : {self.health}")
+        print(f"{self.name} energy : {self.energy}")
 
 class Monster:
-    def __init__(self, health = 100, ):
+    def __init__(self, name, health = 100, ):
+        self.name = name
         self.health = health # dinamis
         self.health_init = self.health # always 500
+        self.damage = 10
     
-    def is_attacked(self):
+    def is_attacked(self, player_name):
+        print(f"{self.name} attack {self.damage} damage to {player_name}")
         return self.health < self.health_init
+    
+    def show_info(self):
+        print(f"{self.name} health : {self.health}")
         
 
 
-player1 = Player()
-player2 = Player()
-oni = Monster(health=500)
+player1 = Player(name="player1")
+player2 = Player(name="player2")
+onis = Monster(name="Oni", health=500)
+reptiles = Monster(name="Reptile")
 
-player1.attack(target=oni, damage=80)
-player2.attack(target=oni, damage=40)
+player1.attack(target=onis, damage=80)
+player2.attack(target=reptiles, damage=40)
 
-oni.attack()
+player1.show_info()
+player2.show_info()
+
+# oni.attack()cls
 
 
 
